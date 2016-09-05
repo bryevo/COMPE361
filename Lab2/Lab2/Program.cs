c6bb49c8e66a5350512b03f74f986d3eabed842c
Brian Vo <ovnairb@gmail.com>
2016-09-04 18:58:20 -0700

    Initial Commit
    Lab2, Assignment1, and SafeAssign Zip Folder

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 69;  //intialized variable set to integer
+            Console.WriteLine("Age: {0} years old", age); //print variable age
+
+            const int MAX_SCORE = 100;
+            int score = 69;
+            float percentage = (float) score/MAX_SCORE; //type casting using (float) before using operator
+            Console.WriteLine("Percentage: {0} %", percentage);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SafeAssign/Assignment1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome Traveler! I will direct you to the fastest route where ever your heart desires!");
+            Console.Write("Enter Your X Coordinate Location in feet: ");
+            float user_pos_x = float.Parse(Console.ReadLine()); //User X input
+            Console.Write("Enter Your Y Coordinate Location in feet: ");
+            float user_pos_y = float.Parse(Console.ReadLine()); //User Y input
+            Console.WriteLine("You are here at x: {0} ft, y: {1} ft", Math.Round(user_pos_x, 3), Math.Round(user_pos_y, 3));
+
+            Console.Write("Where does business take you? Enter X Coordinate: "); //Destination X input
+            float next_pos_x = float.Parse(Console.ReadLine());
+            Console.Write("Enter Y Coordinate: "); //Destination Y input
+            float next_pos_y = float.Parse(Console.ReadLine());
+            Console.WriteLine("You seek to go to position x: {0} ft, y: {1} ft?", Math.Round(next_pos_x, 3), Math.Round(next_pos_y, 3));
+
+            float distance = Distance(user_pos_x, next_pos_x, user_pos_y, next_pos_y); //Calculate distance based on user arguments
+            float angle = Angle(user_pos_x, next_pos_x, user_pos_y, next_pos_y);    //Calculate angle based on user arguments
+            Console.WriteLine("You must travel onward for {0} ft with an angle of {1} degrees.", Math.Round(distance, 3), Math.Round(angle, 3));
+            Console.WriteLine("Stay safe my friend...for the night is dark and full of terrors.");
+            Console.ReadKey();
+        }
+        //Calculate Delta X of input coordinates
+        static float DeltaX(float x1, float x2) {
+            float x = (x2-x1);
+            return x;
+        }
+        //Calculate Delta Y of input coordinates
+        static float DeltaY(float y1, float y2) {
+            float y = (y2 - y1);
+            return y;
+        }
+        //Calculate the distance between 2 points in a cartesian plane
+        static float Distance(float x1, float x2, float y1, float y2) {
+            float result = (float) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return result;
+        }
+        //Calculate the angle between 2 points in a cartesian points.
+        static float Angle(float x1, float x2, float y1, float y2) {
+            float dX = DeltaX(x1, x2);
+            float dY = DeltaY(y1, y2);
+            float radians = (float) Math.Atan2(dY,dX);
+            float angle = (float) (radians * (180 / Math.PI));
+            return angle;
+        }
+        
+    }
+}
