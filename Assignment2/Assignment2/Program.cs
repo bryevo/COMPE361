db59b72e10934f953cf4860cd5fae4c6d6340942
Brian Vo <ovnairb@gmail.com>
2016-09-10 00:49:00 -0700

    Print and Quit method
    displays all elements in text file
    quits the program
    TODO: variable into readable array for method manipulation

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -9,7 +9,20 @@ namespace Assignment2
     class Program
     {
         static void Main(string[] args) {
+            Console.Write("1. All Words\n2. Rhyming words\n3. Scrabble words\n4. Morph words\n5. Quit Program\nYour choice:");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 1){
+                Words word = new Words();
+                word.PrintAll();
+            }
+            if (input == 5) {
+                Quit(input);
+            }
+        }
 
+        public static int Quit(int i) {
+            Console.WriteLine("Done");
+            return 0;
         }
     }
 }
---
+++ Assignment2/Assignment2/Words.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment2
+{
+    public class Words
+    {
+        public void PrintAll() {
+            try {
+                string[] lines = File.ReadAllLines(@"C:/Users/Brye/Desktop/COMPE361/Assignment2/Assignment2/WordList.txt");
+                foreach (string line in lines) {
+                    string dict = line + ",";
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("The file could not be read.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
