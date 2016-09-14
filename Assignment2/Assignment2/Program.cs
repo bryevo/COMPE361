a96532647dba0dae1380caad6c52cbd5e2753d46
Brian Vo <ovnairb@gmail.com>
2016-09-13 22:57:56 -0700

    WIP Assignment 2

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +9,37 @@ namespace Assignment2
 {
     class Program
     {
-        static void Main(string[] args) {
-            Console.Write("1. All Words\n2. Rhyming words\n3. Scrabble words\n4. Morph words\n5. Quit Program\nYour choice:");
-            int input = int.Parse(Console.ReadLine());
-            if (input == 1){
-                Words word = new Words();
-                word.PrintAll();
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Write(
+                    "1. All Words\n2. Rhyming words\n3. Scrabble words\n4. Morph words\n5. Quit Program\nYour choice:");
+                int input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Words word = new Words();
+                        word.PrintAll();
+                        break;
+                    case 2:
+                        Console.WriteLine("Let's see your freestyle! Pick a rhyme scheme: ");
+                        string rhyme = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("You don't know how to rap? Don't worry, I'll search a word that your looking for: ");
+             
+                        break;
+                    case 4:
+                        Console.WriteLine("I'll try to morph your word into something cool");
+                        break;
+                    case 5:
+                        Console.WriteLine("Done");
+                        exit = true;
+                        break;
+                }
             }
-            if (input == 5) {
-                Quit(input);
-            }
-        }
-
-        public static int Quit(int i) {
-            Console.WriteLine("Done");
-            return 0;
         }
     }
 }
--- Assignment2/Assignment2/Words.cs
+++ Assignment2/Assignment2/Words.cs
@@ -5,17 +5,24 @@ namespace Assignment2
 {
     public class Words
     {
-        public void PrintAll() {
-            try {
-                string[] lines = File.ReadAllLines(@"C:/Users/Brye/Desktop/COMPE361/Assignment2/Assignment2/WordList.txt");
-                foreach (string line in lines) {
-                    string dict = line + ",";
-                }
+        private string[] lines;
+        StreamReader reader = new StreamReader(@"..\..\WordList.txt");
+        private string line;
+
+        public Words(string[] list)
+        {
+           
+        }
+
+public void RhymeWord() {
+            try
+            {
+                string rhyme =
             }
             catch (Exception e) {
-                Console.WriteLine("The file could not be read.");
-                Console.WriteLine(e.Message);
+                
             }
+            
         }
     }
 }
