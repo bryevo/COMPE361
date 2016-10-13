90f8782d6f506b641a8aceb3554bc3fc7fc9187a
Brian Vo <ovnairb@gmail.com>
2016-10-13 11:40:57 -0700

    Upload PA4

---
+++ PA4/PA4/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace day12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbWords = new System.Windows.Forms.ListBox();
+            this.btnFill = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.gbWordSearch = new System.Windows.Forms.GroupBox();
+            this.rbMorphChain = new System.Windows.Forms.RadioButton();
+            this.rbMorphWord = new System.Windows.Forms.RadioButton();
+            this.rbScrabble = new System.Windows.Forms.RadioButton();
+            this.rbRhyme = new System.Windows.Forms.RadioButton();
+            this.lbWordSearch = new System.Windows.Forms.ListBox();
+            this.btnFill2 = new System.Windows.Forms.Button();
+            this.btnClear2 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Label();
+            this.tbEnd = new System.Windows.Forms.TextBox();
+            this.end = new System.Windows.Forms.Label();
+            this.tbMax = new System.Windows.Forms.TextBox();
+            this.maxLength = new System.Windows.Forms.Label();
+            this.gbWordSearch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbWords
+            // 
+            this.lbWords.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbWords.FormattingEnabled = true;
+            this.lbWords.ItemHeight = 25;
+            this.lbWords.Location = new System.Drawing.Point(20, 71);
+            this.lbWords.Margin = new System.Windows.Forms.Padding(4);
+            this.lbWords.Name = "lbWords";
+            this.lbWords.Size = new System.Drawing.Size(262, 554);
+            this.lbWords.TabIndex = 0;
+            this.lbWords.DoubleClick += new System.EventHandler(this.lbWords_DoubleClick);
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(20, 19);
+            this.btnFill.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(112, 37);
+            this.btnFill.TabIndex = 1;
+            this.btnFill.Text = "Fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(172, 19);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 37);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUser.Location = new System.Drawing.Point(292, 71);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(394, 31);
+            this.txtUser.TabIndex = 3;
+            this.txtUser.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUser_KeyPress);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.Location = new System.Drawing.Point(696, 69);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(92, 37);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // gbWordSearch
+            // 
+            this.gbWordSearch.Controls.Add(this.rbMorphChain);
+            this.gbWordSearch.Controls.Add(this.rbMorphWord);
+            this.gbWordSearch.Controls.Add(this.rbScrabble);
+            this.gbWordSearch.Controls.Add(this.rbRhyme);
+            this.gbWordSearch.Location = new System.Drawing.Point(292, 284);
+            this.gbWordSearch.Margin = new System.Windows.Forms.Padding(6);
+            this.gbWordSearch.Name = "gbWordSearch";
+            this.gbWordSearch.Padding = new System.Windows.Forms.Padding(6);
+            this.gbWordSearch.Size = new System.Drawing.Size(242, 223);
+            this.gbWordSearch.TabIndex = 5;
+            this.gbWordSearch.TabStop = false;
+            this.gbWordSearch.Text = "Options:";
+            // 
+            // rbMorphChain
+            // 
+            this.rbMorphChain.AutoSize = true;
+            this.rbMorphChain.Location = new System.Drawing.Point(14, 158);
+            this.rbMorphChain.Name = "rbMorphChain";
+            this.rbMorphChain.Size = new System.Drawing.Size(166, 29);
+            this.rbMorphChain.TabIndex = 3;
+            this.rbMorphChain.TabStop = true;
+            this.rbMorphChain.Text = "Morph Chain";
+            this.rbMorphChain.UseVisualStyleBackColor = true;
+            // 
+            // rbMorphWord
+            // 
+            this.rbMorphWord.AutoSize = true;
+            this.rbMorphWord.Location = new System.Drawing.Point(14, 120);
+            this.rbMorphWord.Margin = new System.Windows.Forms.Padding(6);
+            this.rbMorphWord.Name = "rbMorphWord";
+            this.rbMorphWord.Size = new System.Drawing.Size(161, 29);
+            this.rbMorphWord.TabIndex = 2;
+            this.rbMorphWord.TabStop = true;
+            this.rbMorphWord.Text = "Morph Word";
+            this.rbMorphWord.UseVisualStyleBackColor = true;
+            // 
+            // rbScrabble
+            // 
+            this.rbScrabble.AutoSize = true;
+            this.rbScrabble.Location = new System.Drawing.Point(14, 79);
+            this.rbScrabble.Margin = new System.Windows.Forms.Padding(6);
+            this.rbScrabble.Name = "rbScrabble";
+            this.rbScrabble.Size = new System.Drawing.Size(128, 29);
+            this.rbScrabble.TabIndex = 1;
+            this.rbScrabble.TabStop = true;
+            this.rbScrabble.Text = "Scrabble";
+            this.rbScrabble.UseVisualStyleBackColor = true;
+            // 
+            // rbRhyme
+            // 
+            this.rbRhyme.AutoSize = true;
+            this.rbRhyme.Location = new System.Drawing.Point(14, 38);
+            this.rbRhyme.Margin = new System.Windows.Forms.Padding(6);
+            this.rbRhyme.Name = "rbRhyme";
+            this.rbRhyme.Size = new System.Drawing.Size(110, 29);
+            this.rbRhyme.TabIndex = 0;
+            this.rbRhyme.TabStop = true;
+            this.rbRhyme.Text = "Rhyme";
+            this.rbRhyme.UseVisualStyleBackColor = true;
+            // 
+            // lbWordSearch
+            // 
+            this.lbWordSearch.FormattingEnabled = true;
+            this.lbWordSearch.ItemHeight = 25;
+            this.lbWordSearch.Location = new System.Drawing.Point(548, 177);
+            this.lbWordSearch.Margin = new System.Windows.Forms.Padding(6);
+            this.lbWordSearch.Name = "lbWordSearch";
+            this.lbWordSearch.Size = new System.Drawing.Size(236, 429);
+            this.lbWordSearch.TabIndex = 6;
+            // 
+            // btnFill2
+            // 
+            this.btnFill2.Location = new System.Drawing.Point(548, 121);
+            this.btnFill2.Margin = new System.Windows.Forms.Padding(6);
+            this.btnFill2.Name = "btnFill2";
+            this.btnFill2.Size = new System.Drawing.Size(110, 44);
+            this.btnFill2.TabIndex = 7;
+            this.btnFill2.Text = "Fill";
+            this.btnFill2.UseVisualStyleBackColor = true;
+            this.btnFill2.Click += new System.EventHandler(this.btnFill2_Click);
+            // 
+            // btnClear2
+            // 
+            this.btnClear2.Location = new System.Drawing.Point(670, 121);
+            this.btnClear2.Margin = new System.Windows.Forms.Padding(6);
+            this.btnClear2.Name = "btnClear2";
+            this.btnClear2.Size = new System.Drawing.Size(118, 44);
+            this.btnClear2.TabIndex = 8;
+            this.btnClear2.Text = "Clear";
+            this.btnClear2.UseVisualStyleBackColor = true;
+            this.btnClear2.Click += new System.EventHandler(this.btnClear2_Click);
+            // 
+            // start
+            // 
+            this.start.AutoSize = true;
+            this.start.Location = new System.Drawing.Point(291, 106);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(114, 25);
+            this.start.TabIndex = 9;
+            this.start.Text = "Start Word";
+            // 
+            // tbEnd
+            // 
+            this.tbEnd.Location = new System.Drawing.Point(292, 134);
+            this.tbEnd.Name = "tbEnd";
+            this.tbEnd.Size = new System.Drawing.Size(146, 31);
+            this.tbEnd.TabIndex = 10;
+            // 
+            // end
+            // 
+            this.end.AutoSize = true;
+            this.end.Location = new System.Drawing.Point(291, 172);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(107, 25);
+            this.end.TabIndex = 11;
+            this.end.Text = "End Word";
+            // 
+            // tbMax
+            // 
+            this.tbMax.Location = new System.Drawing.Point(292, 200);
+            this.tbMax.Name = "tbMax";
+            this.tbMax.Size = new System.Drawing.Size(100, 31);
+            this.tbMax.TabIndex = 12;
+            // 
+            // maxLength
+            // 
+            this.maxLength.AutoSize = true;
+            this.maxLength.Location = new System.Drawing.Point(287, 234);
+            this.maxLength.Name = "maxLength";
+            this.maxLength.Size = new System.Drawing.Size(125, 25);
+            this.maxLength.TabIndex = 13;
+            this.maxLength.Text = "Max Length";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 654);
+            this.Controls.Add(this.maxLength);
+            this.Controls.Add(this.tbMax);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.tbEnd);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.btnClear2);
+            this.Controls.Add(this.btnFill2);
+            this.Controls.Add(this.lbWordSearch);
+            this.Controls.Add(this.gbWordSearch);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFill);
+            this.Controls.Add(this.lbWords);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Word Search";
+            this.gbWordSearch.ResumeLayout(false);
+            this.gbWordSearch.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbWords;
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.GroupBox gbWordSearch;
+        private System.Windows.Forms.RadioButton rbMorphWord;
+        private System.Windows.Forms.RadioButton rbScrabble;
+        private System.Windows.Forms.RadioButton rbRhyme;
+        private System.Windows.Forms.ListBox lbWordSearch;
+        private System.Windows.Forms.Button btnFill2;
+        private System.Windows.Forms.Button btnClear2;
+        private System.Windows.Forms.RadioButton rbMorphChain;
+        private System.Windows.Forms.Label start;
+        private System.Windows.Forms.TextBox tbEnd;
+        private System.Windows.Forms.Label end;
+        private System.Windows.Forms.TextBox tbMax;
+        private System.Windows.Forms.Label maxLength;
+    }
+}
+
---
+++ PA4/PA4/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day12
+{
+    public partial class Form1 : Form
+    {
+        Words word = new Words();
+        String[] List = PA4.Properties.Resources.WordList.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Additional initialization
+            lbWords.Sorted = true;
+            lbWordSearch.Sorted = true;
+        }
+
+        private bool findText(string text_str)
+        {
+            bool found = false;
+
+            foreach (object lbItem in lbWords.Items)
+            {
+                if (lbItem.ToString().StartsWith(text_str, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    lbWords.SelectedIndex = lbWords.Items.IndexOf(lbItem);
+                    found = true;
+                    break;
+                }
+            }
+
+            return found;
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnFill_Click");
+
+            #region The Old Way
+#if false
+            string s;
+            FileStream fs;
+
+            try
+            {
+                fs = new FileStream(@"..\..\WordList.txt", FileMode.Open, FileAccess.Read);
+            }
+            catch
+            {
+                MessageBox.Show("Unable to open WordList.txt");
+                return;
+            }
+
+            StreamReader sr = new StreamReader(fs);
+
+            lbWords.BeginUpdate();
+            lbWords.Items.Clear();
+            while ((s = sr.ReadLine()) != null)
+            {
+                lbWords.Items.Add(s);
+            }
+            lbWords.EndUpdate();
+#endif
+            #endregion
+
+            lbWords.BeginUpdate();
+            lbWords.Items.Clear();
+            foreach (string lines in List)
+                lbWords.Items.Add(lines);
+            lbWords.EndUpdate();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnClear_Click");
+            lbWords.Items.Clear();
+        }
+
+        private void txtUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("txtUser_KeyPress '{0}'", e.KeyChar);
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                if (!findText(txtUser.Text))
+                    lbWords.Items.Add(txtUser.Text);
+                else
+                {
+                    string msgStr = String.Format("String '{0}' already exists!", txtUser.Text);
+                    MessageBox.Show(msgStr);
+                }
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            bool found = false;
+
+            Console.WriteLine("btnFind_Click");
+
+            found = findText(txtUser.Text);
+            if (!found)
+            {
+                string msgStr = String.Format("Couldn't find the string '{0}'", txtUser.Text);
+                MessageBox.Show(msgStr);
+            }
+        }
+
+        private void lbWords_DoubleClick(object sender, EventArgs e)
+        {
+            txtUser.Text = lbWords.Text;
+        }
+
+        private void btnClear2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnClear2_Click");
+            lbWordSearch.Items.Clear();
+        }
+
+        private void btnFill2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnFill2_Click");
+
+            if (rbRhyme.Checked)
+            {
+                lbWordSearch.BeginUpdate();
+                lbWordSearch.Items.Clear();
+                foreach (string line in List)
+                {
+                    if (line.EndsWith(txtUser.Text))
+                    {
+                        lbWordSearch.Items.Add(line);
+                    }
+                }
+                lbWordSearch.EndUpdate();
+            }
+            else if (rbScrabble.Checked)
+            {
+                lbWordSearch.BeginUpdate();
+                lbWordSearch.Items.Clear();
+                List<string> wordScrabble = word.ScrabbleWord(txtUser.Text);
+                foreach (string scrabble in wordScrabble)
+                {
+                    if (scrabble != wordScrabble[0])
+                        lbWordSearch.Items.Add(scrabble);
+                }
+                lbWordSearch.EndUpdate();
+            }
+            else if (rbMorphWord.Checked)
+            {
+                lbWordSearch.BeginUpdate();
+                lbWordSearch.Items.Clear();
+                string[] wordMorph = word.MorphWord(txtUser.Text);
+                foreach (string morph in wordMorph)
+                {
+                        lbWordSearch.Items.Add(morph);
+                }
+                lbWordSearch.EndUpdate();
+            }
+            else if (rbMorphChain.Checked)
+            {
+                lbWordSearch.BeginUpdate();
+                lbWordSearch.Items.Clear();
+                int maxLength = Int32.Parse(tbMax.Text);
+                List<string> wordChain = word.MorphChain(txtUser.Text, tbEnd.Text, maxLength);
+                foreach (string chain in wordChain)
+                {
+                    lbWordSearch.Sorted = false;
+                    lbWordSearch.Items.Add(chain);
+                }
+                lbWordSearch.EndUpdate();
+            }
+        }
+    }
+}
