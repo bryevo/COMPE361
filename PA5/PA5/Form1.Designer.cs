3929f775cb538da6011ebe4b054e75b8eb3087a5
Brian Vo <ovnairb@gmail.com>
2016-10-14 17:58:14 -0700

    Design UI PA5
    todo: implement dropdown for date/timer picker
    link to add button

---
+++ PA5/PA5/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PA5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAddAlarm = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbAlarm = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbAppointment = new System.Windows.Forms.CheckedListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.timer_Time = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAddAlarm
+            // 
+            this.btnAddAlarm.ForeColor = System.Drawing.Color.Black;
+            this.btnAddAlarm.Location = new System.Drawing.Point(157, 51);
+            this.btnAddAlarm.Name = "btnAddAlarm";
+            this.btnAddAlarm.Size = new System.Drawing.Size(31, 24);
+            this.btnAddAlarm.TabIndex = 2;
+            this.btnAddAlarm.Text = "+";
+            this.btnAddAlarm.UseVisualStyleBackColor = true;
+            this.btnAddAlarm.Click += new System.EventHandler(this.btnAddAlarm_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Black;
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.cbAlarm);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.btnAddAlarm);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(13, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 329);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Alarm";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "MM/dd/yy  hh:mm:ss tt";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 21);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(144, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.Value = new System.DateTime(2016, 10, 14, 17, 42, 56, 0);
+            // 
+            // cbAlarm
+            // 
+            this.cbAlarm.BackColor = System.Drawing.Color.Black;
+            this.cbAlarm.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cbAlarm.ForeColor = System.Drawing.Color.White;
+            this.cbAlarm.FormattingEnabled = true;
+            this.cbAlarm.Location = new System.Drawing.Point(7, 56);
+            this.cbAlarm.Name = "cbAlarm";
+            this.cbAlarm.Size = new System.Drawing.Size(144, 238);
+            this.cbAlarm.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(157, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Black;
+            this.groupBox2.Controls.Add(this.cbAppointment);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.groupBox2.Location = new System.Drawing.Point(213, 50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 329);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Appointments";
+            // 
+            // cbAppointment
+            // 
+            this.cbAppointment.BackColor = System.Drawing.Color.Black;
+            this.cbAppointment.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cbAppointment.ForeColor = System.Drawing.Color.White;
+            this.cbAppointment.FormattingEnabled = true;
+            this.cbAppointment.Location = new System.Drawing.Point(6, 21);
+            this.cbAppointment.Name = "cbAppointment";
+            this.cbAppointment.Size = new System.Drawing.Size(145, 272);
+            this.cbAppointment.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(157, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(31, 24);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(157, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Bodoni MT", 24F);
+            this.lbTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbTime.Location = new System.Drawing.Point(13, 9);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(0, 38);
+            this.lbTime.TabIndex = 4;
+            // 
+            // timer_Time
+            // 
+            this.timer_Time.Enabled = true;
+            this.timer_Time.Interval = 1000;
+            this.timer_Time.Tick += new System.EventHandler(this.timer_Time_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(156, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(31, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(424, 391);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Program Assignment 5";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddAlarm;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.CheckedListBox cbAlarm;
+        private System.Windows.Forms.CheckedListBox cbAppointment;
+        private System.Windows.Forms.Timer timer_Time;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ PA5/PA5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbTime.Text = DateTime.Now.ToString();  //Current Time
+        }
+        /// <summary>
+        /// Using timer to continously update the time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Time_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToString();
+        }
+
+        private void btnAddAlarm_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
