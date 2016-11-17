f95ebe8761d74f8f2354cf96d9d971fa74c3559b
Brian Vo <ovnairb@gmail.com>
2016-11-16 16:08:24 -0800

    Labs 5-7

---
+++ Lab 6 - Move the Bears/Lab 6 - Move the Bears/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_6___Move_the_Bears
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
